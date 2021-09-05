30f146596d831fa613665339c40b9034b8596f19
dnjsrud3407 <dnjsrud3407@naver.com>
2021-09-05 19:19:38 +0900

    webSecurityConfig 수정

--- nubila/src/main/java/com/nubila/nubila/config/WebSecurityConfig.java
+++ nubila/src/main/java/com/nubila/nubila/config/WebSecurityConfig.java
@@ -6,21 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -47,7 +38,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers(urlPermitAll())
                         .permitAll()
                     // 2. 특정 권한에게 Open되는 곳
-                    .antMatchers(urlPermitUser()).hasRole("USER")
                     .antMatchers("/admin").hasRole("ADMIN")
                     // 3. 그 밖의 페이지는 모두 로그인 필요
                     .anyRequest()
@@ -73,19 +63,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     public String[] urlPermitAll() {
         String[] urls = new String[] {
-                "/", "/nubila", "/support", "/search",
-                "/user/login", "/user/join", "/user/idCheck", "/user/emailCheck", "/user/idSearchForm",
-                "/user/idSearchResult", "/user/pwSearchForm", "/user/sendMail", "/user/pwChangeForm",
-        };
-        return urls;
-    }
-
-    public String[] urlPermitUser() {
-        String[] urls = new String[] {
-                "/bookmark/**",
-                "/user/myInfo", "/user/pwConfirm", "/user/myInfoPwChange", "/user/myInfoPwChangeForm",
-                "/user/emailConfirm", "/user/myInfoEmailChange", "/user/myInfoEmailChangeForm",
-                "/user/delete", "/user/pwConfirmAndDelete", "/user/deleteOk"
+                "/", "/nubila", "/support", "/search", "/nubija", "/accessDenied",
+                "/user/**"
         };
         return urls;
     }
