7297648908fcb29a7431b2a77d6f23f90b17250a
dnjsrud3407 <dnjsrud3407@naver.com>
2021-09-08 00:17:59 +0900

    NubilaApplication 수정

--- nubila/src/main/java/com/nubila/nubila/NubilaApplication.java
+++ nubila/src/main/java/com/nubila/nubila/NubilaApplication.java
@@ -2,25 +2,13 @@ package com.nubila.nubila;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-
-
-// 메인 Configuration 위치에서 하위 패키지는 디폴트로 스캔하므로
-// 따로 스캔할 패키지 위치를 지정할 필요가 없는 것 같음.
-//@ComponentScan(basePackages = {"com.nubila.nubila.bookmark"})
-//@MapperScan(basePackages = {"com.nubila.nubila.bookmark"})
 
 @SpringBootApplication
 public class NubilaApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "/home/ec2-user/app/config/springboot-webservice/application.properties,"
-			+ "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(NubilaApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(NubilaApplication.class, args);
 	}
+
 
 }
