d64eae17ed93f4c8dbb491a99d754ec9aad0b93f
dnjsrud3407 <dnjsrud3407@naver.com>
2021-09-09 16:37:23 +0900

    NubilaApplication 수정

--- nubila/src/main/java/com/nubila/nubila/NubilaApplication.java
+++ nubila/src/main/java/com/nubila/nubila/NubilaApplication.java
@@ -1,19 +1,13 @@
 package com.nubila.nubila;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NubilaApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "/home/ec2-user/app/config/springboot-webservice/application.properties,"
-	    + "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
-
     public static void main(String[] args) {
-        String path = System.getProperty("user.dir");
-        new SpringApplicationBuilder(NubilaApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(NubilaApplication.class, args);
     }
 }
